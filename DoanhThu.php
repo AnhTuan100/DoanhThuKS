2a81434e714f80c3d11543e77519506a17733142
Trịnh Tuấn QNU <dtuan105205@gmail.com>
2025-05-26 12:33:52 +0700

    .

--- DoanhThu.php
+++ DoanhThu.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include 'ketnoi.php';
 include 'kiemtra.php';
+include 'ketnoi.php';
 include 'xulySQL.php';
 ?>
 
--- footer.php
+++ footer.php
@@ -6,13 +6,13 @@
                     <a href="#"><i class="fa-brands fa-twitter"></i> Twitter</a>
                     <a href="#"><i class="fa-brands fa-instagram"></i> Instagram</a>
                 </div>
-                <div class="chantrang2 huongdan">
+                <div class="chantrang2">
                     <h4 class="guide">CHI TIẾT</h4>
                     <a href="#">Giới thiệu</a>
                     <a href="#">Điều khoản sử dụng</a>
                     <a href="#">Chính sách bảo mật</a>
                 </div> 
-                <div class="chantrang3 ketnoi">
+                <div class="chantrang3">
                     <h4 class="contact">CONTACT US</h4>
                     <p>Email: support@gmail.com</p>
                     <p>Phone: +84 123 456 789</p>
